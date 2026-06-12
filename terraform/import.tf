cb751989fe90e2079a5d7baeb0899a19b0bc031f
Juan <juan_Century@hotmail.com>
2026-06-12 17:03:05 -0300

    fix terraform bd already exist

--- terraform/import.tf
+++ terraform/import.tf
@@ -2,3 +2,8 @@ import {
   id = "projects/${var.project_id}/instances/emil-ai-postgres-${var.environment}"
   to = google_sql_database_instance.db
 }
+
+import {
+  id = "projects/${var.project_id}/instances/emil-ai-postgres-${var.environment}/databases/emil_ai_db"
+  to = google_sql_database.database
+}
